baa950f5f00c2d0b959745f7cb2d7e7786c84c28
Ronald Miranda <ronaldmirandasv@gmail.com>
2020-08-26 03:34:49 -0600

    commit inicial

---
+++ README.md
@@ -0,0 +1,5 @@
+# School Management (Administración escolar)
+
+Administración escolar es un sistema de gestión de estudiantes que puede ser utilizado por instituciones educativas para gestionar la información de sus estudiantes. 
+
+## Aun en desarrollo.... 
---
+++ install.sql
@@ -0,0 +1,122 @@
+CREATE TABLE cycles(
+	id BIGINT UNSIGNED AUTO_INCREMENT,
+	name VARCHAR(20) NOT NULL,
+	created_at DATETIME NOT NULL,
+	updated_at DATETIME NOT NULL,
+	closed_at DATETIME,
+	PRIMARY KEY (id)
+) ENGINE=InnoDB
+COMMENT="Ciclos escolares";
+
+CREATE TABLE classrooms (
+	id BIGINT UNSIGNED AUTO_INCREMENT,
+	name VARCHAR(20) NOT NULL,
+	description VARCHAR(50),
+	created_at DATETIME NOT NULL,
+	updated_at DATETIME NOT NULL,
+	PRIMARY KEY (id)
+) ENGINE=InnoDB
+COMMENT="Catalogo de salones de clases";
+
+CREATE TABLE users (
+	id BIGINT UNSIGNED AUTO_INCREMENT,
+	name VARCHAR(40) NOT NULL,
+	last_name VARCHAR(40) NOT NULL,
+	birthday DATE NOT NULL,
+	username VARCHAR(20) NOT NULL,
+	`password` VARCHAR(255) NOT NULL,
+	email VARCHAR(255),
+	active BOOLEAN NOT NULL DEFAULT TRUE,
+	created_at DATETIME NOT NULL,
+	updated_at DATETIME NOT NULL,
+	PRIMARY KEY (id),
+	CONSTRAINT uq_users_username UNIQUE (username),
+	CONSTRAINT uq_users_email UNIQUE (email)
+) ENGINE=InnoDB
+COMMENT="Usuarios del sistema";
+
+CREATE TABLE roles (
+	id INT UNSIGNED AUTO_INCREMENT,
+	`role` VARCHAR(40) NOT NULL,
+	PRIMARY KEY (id),
+	CONSTRAINT uq_roles_rol UNIQUE (`role`)
+) ENGINE=InnoDB
+COMMENT="Catalogo de roles";
+
+CREATE TABLE user_roles (
+	id BIGINT UNSIGNED AUTO_INCREMENT,
+	user_id BIGINT UNSIGNED NOT NULL,
+	role_id INT UNSIGNED NOT NULL,
+	PRIMARY KEY (id),
+	CONSTRAINT fk_user_roles_user_id FOREIGN KEY (user_id) REFERENCES users(id),
+	CONSTRAINT fk_user_roels_role_id FOREIGN KEY (role_id) REFERENCES roles(id)
+) ENGINE=InnoDB
+COMMENT="Roles asociados al usuario";
+
+CREATE TABLE courses(
+	id BIGINT UNSIGNED AUTO_INCREMENT,
+	name VARCHAR(50) NOT NULL,
+	created_at DATETIME NOT NULL,
+	updated_at DATETIME NOT NULL,
+	in_charge_user_id BIGINT UNSIGNED,
+	cycle_id BIGINT UNSIGNED NOT NULL,
+	classroom_id BIGINT UNSIGNED,
+	primary key (id),
+	CONSTRAINT fk_courses_cycle_id FOREIGN KEY (cycle_id) REFERENCES cycles(id),
+	CONSTRAINT fk_courses_classroom_id FOREIGN KEY (classroom_id) REFERENCES classrooms(id)
+) ENGINE=InnoDB
+COMMENT="Cursos de cada ciclo escolar";
+
+CREATE TABLE course_students(
+	id BIGINT UNSIGNED AUTO_INCREMENT,
+	course_id BIGINT UNSIGNED NOT NULL,
+	student_id BIGINT UNSIGNED NOT NULL,
+	PRIMARY KEY (id),
+	CONSTRAINT fk_course_id FOREIGN KEY (course_id) REFERENCES courses (id),
+	CONSTRAINT fk_student_id FOREIGN KEY (student_id) REFERENCES users (id)
+) ENGINE=InnoDB
+COMMENT="Estudiantes suscritos a cursos";
+
+CREATE TABLE subjects (
+	id BIGINT UNSIGNED AUTO_INCREMENT,
+	name VARCHAR(50) NOT NULL,
+	course_id BIGINT UNSIGNED NOT NULL,
+	classroom_id BIGINT UNSIGNED NOT NULL,
+	starting_at DATETIME NOT NULL,
+	ending_at DATETIME NOT NULL,
+	teacher_id BIGINT UNSIGNED NOT NULL,
+	PRIMARY KEY (id),
+	CONSTRAINT fk_subjects_course_id FOREIGN KEY (course_id) REFERENCES courses(id),
+	CONSTRAINT fk_subjects_classroom_id FOREIGN KEY (classroom_id) REFERENCES classrooms(id),
+	CONSTRAINT fk_subjects_teacher_id FOREIGN KEY (teacher_id) REFERENCES users(id)
+) ENGINE=InnoDB
+COMMENT="Materias registradas a cursos";
+
+CREATE TABLE grades (
+	id BIGINT UNSIGNED AUTO_INCREMENT,
+	subject_id BIGINT UNSIGNED NOT NULL,
+	student_id BIGINT UNSIGNED NOT NULL,
+	grade DECIMAL(4, 2) NOT NULL,
+	description VARCHAR(50),
+	created_at DATETIME NOT NULL,
+	updated_at DATETIME NOT NULL,
+	deleted_at DATETIME,
+	delete_comment VARCHAR(50),
+	PRIMARY KEY (id),
+	CONSTRAINT fk_grades_subject_id FOREIGN KEY (subject_id) REFERENCES subjects(id),
+	CONSTRAINT fk_grades_student_id FOREIGN KEY (student_id) REFERENCES users(id)
+) ENGINE=InnoDB
+COMMENT="Notas de cada materia";
+
+insert into roles (role) values ("ROLE_ADMIN");
+insert into roles (role) values ("ROLE_STUDENT");
+insert into roles (role) values ("ROLE_TEACHER");
+insert into roles (role) values ("ROLE_STUDENT_DATA_EDITOR");
+
+insert into users 
+(name, last_name, birthday, username, `password`, email, active, created_at, updated_at) 
+values 
+("Admin", "System", now(), "system", "$2a$10$5dYDQRJaGLMIiXv49goz0uGdZCmrZQ1shakWGy0446SE09dSwqbA2",
+"admin@sms.com", true, now(), now());
+
+insert into user_roles (user_id, role_id) values (1, 1);
