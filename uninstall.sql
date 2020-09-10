c0c49e941e2ca2ba1ce3d854946f1998a53f23c2
Ronald Miranda <ronaldmirandasv@gmail.com>
2020-09-10 02:23:25 -0600

    script para eliminar las tablas de la base de datos

---
+++ uninstall.sql
@@ -0,0 +1,9 @@
+DROP TABLE grades;
+DROP TABLE subjects;
+DROP TABLE course_students;
+DROP TABLE courses;
+DROP TABLE user_roles;
+DROP TABLE roles;
+DROP TABLE users;
+DROP TABLE classrooms;
+DROP TABLE cycles;
