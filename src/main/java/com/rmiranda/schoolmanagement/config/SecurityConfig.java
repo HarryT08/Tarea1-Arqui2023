c3997cc5c1cbb262b44e8c75a0abcfe2a5ac8d34
Ronald Miranda <ronaldmirandasv@gmail.com>
2020-10-20 20:38:28 -0600

    custom login page agregado y logica de logout modificada

--- src/main/java/com/rmiranda/schoolmanagement/config/SecurityConfig.java
+++ src/main/java/com/rmiranda/schoolmanagement/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -19,8 +20,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/css/**", "/js/**").permitAll().anyRequest().authenticated().and()
-                .formLogin().permitAll().and().logout().permitAll();
+        http.authorizeRequests()
+            .antMatchers("/css/**", "/js/**")
+            .permitAll()
+            .anyRequest()
+            .authenticated()
+            .and()
+            .formLogin()
+            .loginPage("/login")
+            .permitAll()
+            .and()
+            .logout()
+            .logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login?logout")
+            .permitAll();
     }
 
     @Override
