6030d6abc89e9ed1da593525fa57036c84aa2300
Ronald Miranda <ronaldmirandasv@gmail.com>
2020-11-04 15:05:59 -0600

    remove unused imports

--- src/main/java/com/rmiranda/schoolmanagement/config/SecurityConfig.java
+++ src/main/java/com/rmiranda/schoolmanagement/config/SecurityConfig.java
@@ -4,11 +4,8 @@ import com.rmiranda.schoolmanagement.handler.CustomAuthenticationFailureHandler;
 import com.rmiranda.schoolmanagement.service.UserDetailService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationEventPublisher;
-import org.springframework.security.authentication.DefaultAuthenticationEventPublisher;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
